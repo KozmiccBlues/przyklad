30316367554fe6cbcf0ec254d31a78fddbd56499
Ola <kozlowska.aleksandra92@gmail.com>
2021-10-22 18:36:05 +0200

    Dodanie wykrzyknika

--- src/Main2.java
+++ src/Main2.java
@@ -1,7 +1,7 @@
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Witaj świecie");
-        System.out.println("Witaj świecie");
+        System.out.println("Witaj świecie!");
     }
 
 }
